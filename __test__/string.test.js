cb5cfc334a4089ad26249007a8aa4b1f1559c1ea
sundicide <kyusuen@gmail.com>
2023-03-15 08:43:04 +0900

    refactor: var to let

--- __test__/string.test.js
+++ __test__/string.test.js
@@ -1,7 +1,7 @@
 import { expectEqualTest, LOG } from './utils'
 
 test('replace test', () => {
-  var str = 'abc'
+  let str = 'abc'
   str.replace('b', 'd')
   expectEqualTest(str, 'abc') // 원본은 안바뀐다.
 
@@ -12,8 +12,8 @@ test('replace test', () => {
 // 문자열을 array로 만들기 위해 split을 사용할 필요 없이
 // for loop로 사용 가능
 test('for loop test', () => {
-  var str = 'ABCDEFGHI'
-  var result = ''
+  let str = 'ABCDEFGHI'
+  let result = ''
   for (let x of str) {
     result += x
   }
