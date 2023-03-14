323bd03aa0a8a5ebab60685e38b3bbbac60749f9
sundicide <kyusuen@gmail.com>
2023-03-15 08:41:52 +0900

    update string test

--- __test__/string.test.js
+++ __test__/string.test.js
@@ -1,18 +1,21 @@
-import { expectEqualTest, LOG } from "./utils";
+import { expectEqualTest, LOG } from './utils'
 
-test("replace test", () => {
-  var a = "abc";
-  a.replace("b", "d");
-  expectEqualTest(a, "abc"); // 원본은 안바뀐다.
-});
+test('replace test', () => {
+  var str = 'abc'
+  str.replace('b', 'd')
+  expectEqualTest(str, 'abc') // 원본은 안바뀐다.
+
+  const newStr = str.replace('b', 'd')
+  expectEqualTest(newStr, 'adc')
+})
 
 // 문자열을 array로 만들기 위해 split을 사용할 필요 없이
 // for loop로 사용 가능
-test("for loop test", () => {
-  var s = "ABCDEFGHI";
-  var result = "";
-  for (let x of s) {
-    result += x;
+test('for loop test', () => {
+  var str = 'ABCDEFGHI'
+  var result = ''
+  for (let x of str) {
+    result += x
   }
-  expectEqualTest(s, result);
-});
+  expectEqualTest(str, result)
+})
