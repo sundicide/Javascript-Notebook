52f987f191b2ed83a5f487079695e48e8242db70
sundicide <kyusuen@gmail.com>
2023-03-15 08:36:20 +0900

    update set test

--- __test__/set.test.js
+++ __test__/set.test.js
@@ -1,8 +1,18 @@
-test("set test", () => {
-    const a = new Set()
-    a.add("a")
-    a.add("b")
-    a.add("b")
-    console.log(a.has("a"))
-    console.log(a)
+import { expectEqualTest, LOG } from './utils'
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
+
+test('set test', () => {
+  const mySet = new Set()
+
+  mySet.add(1)
+
+  expectEqualTest(mySet.delete(1), true)
+  expectEqualTest(mySet.delete(2), false)
+
+  mySet.add(3)
+  mySet.add(3)
+
+  expectEqualTest(mySet.size, 1)
+  expectEqualTest(mySet.has(3), true)
 })
